f2bbfac43dc6f3de69ab20b750baf93847ef8b91
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-11-14 11:39:57 +0100

    feat(base): add vim
    Added vim as it is used as default editor in a lot of scripts / programs

--- install-base.sh
+++ install-base.sh
@@ -1,6 +1,6 @@
 # Basic packages
 sudo pacman -S --noconfirm --needed bat btop chezmoi curl eza fastfetch fd fzf \
-go-yq jq meld neovim ripgrep starship tealdeer tmux unzip wget zellij zoxide
+go-yq jq meld neovim ripgrep starship tealdeer tmux unzip vim wget zellij zoxide
 
 # Git tools
 sudo pacman -S --noconfirm --needed git git-delta github-cli lazygit
@@ -15,3 +15,23 @@ sudo pacman -S --noconfirm --needed flatpak
 # Pacman stuff
 sudo pacman -S --needed --noconfirm pacman-contrib
 sudo systemctl enable paccache.timer
+## pacman hook for grub update
+if [ ! -d /etc/pacman.d/hooks ]; then
+  sudo mkdir -p /etc/pacman.d/hooks
+fi
+
+if [ ! -f /etc/pacman.d/hooks/91-update-grub.hook ]; then
+  cat <<EOF | sudo tee -a /etc/pacman.d/hooks/91-update-grub.hook
+[Trigger]
+Type = File
+Operation = Install
+Operation = Upgrade
+Operation = Remove
+Target = usr/lib/modules/*/vmlinuz
+
+[Action]
+Description = Updating grub configuration ...
+When = PostTransaction
+Exec = /usr/bin/grub-mkconfig -o /boot/grub/grub.cfg
+  EOF
+fi
