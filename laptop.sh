b090e8a65bbf8443a2f6c8ce9f1fa6159616b74f
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-11-18 09:52:30 +0100

    feat: add laptop tools

---
+++ laptop.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Remove power-profiles-daemon to avoid conflict with tlp
+sudo pacman -R power-profiles-daemon
+# Install tlp 
+# https://wiki.archlinux.org/title/TLP
+sudo pacman -S --needed --noconfirm tlp
+
+# Enable and start tlp service
+sudo systemctl enable tlp.service
+sudo systemctl start tlp.service
+
+# Mask systemd-rfkill service and socket (see Arch Wiki)
+sudo systemctl mask systemd-rfkill.service
+sudo systemctl mask systemd-rfkill.socket
+
