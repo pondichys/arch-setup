160074674f58ceb3d7b3217d973b49e558519968
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-11-22 10:29:43 +0100

    feat(awesome): [WIP] install Awesome window manager

---
+++ awesomewm.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo pacman -S --needed --noconfirm awesome
+
+sudo pacman -S --needed --noconfirm lxappearance
+sudo pacman -S --needed --noconfirm network-manager-applet polkit-gnome picom blueman
+
+# Other stuff to check
+sudo pacman -S --needed --noconfirm pcmanfm-qt lxqt-archiver gvfs flameshot
