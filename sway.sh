7503c8de397f6839062c30e76b4ba7de3cd1657d
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-11-26 13:57:02 +0100

    feat(sway): initial commit

---
+++ sway.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+# Script to install fully functional Sway environment on barebone archlinux install
+sudo pacman -S sway swaylock swayidle
+
+# Nice tools
+sudo pacman -S --needed --noconfirm wev
