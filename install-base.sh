43eb45fc93553c97f13e7aec1c96f54f1ed32941
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2025-02-22 09:51:30 +0100

    fix: cleanup yay-bin directory after install

--- install-base.sh
+++ install-base.sh
@@ -2,14 +2,14 @@
 
 # Basic packages
 sudo pacman -S --noconfirm --needed bat btop chezmoi curl eza fastfetch fd fzf \
-go-yq jq meld neovim ripgrep starship tealdeer tmux unzip vim wget zellij zoxide
+  go-yq jq meld neovim ripgrep starship tealdeer tmux unzip vim wget zellij zoxide
 
 # Git tools
 sudo pacman -S --noconfirm --needed git git-delta github-cli lazygit
 
 # Shells
 sudo pacman -S --noconfirm --needed bash bash-completion
-sudo pacman -S --noconfirm --needed zsh zsh-completions
+#sudo pacman -S --noconfirm --needed zsh zsh-completions
 sudo pacman -S --noconfirm --needed fish
 
 # Flatpak
@@ -21,50 +21,53 @@ sudo pacman -S --needed --noconfirm pacman-contrib
 sudo systemctl enable paccache.timer
 
 # Only run this if on pure archlinux
-os_release=$(grep -e "^ID=" /etc/os-release | cut -d'=' -f2)
-if [ ${os_release} = "arch" ]; then
-## pacman hook for grub update
-	if [ ! -d /etc/pacman.d/hooks ]; then
-  		sudo mkdir -p /etc/pacman.d/hooks
-	fi
+# os_release=$(grep -e "^ID=" /etc/os-release | cut -d'=' -f2)
+# if [ ${os_release} = "arch" ]; then
+# ## pacman hook for grub update
+# 	if [ ! -d /etc/pacman.d/hooks ]; then
+#   		sudo mkdir -p /etc/pacman.d/hooks
+# 	fi
 
-	if [ ! -f /etc/pacman.d/hooks/91-update-grub.hook ]; then
-  		cat <<EOF | sudo tee -a /etc/pacman.d/hooks/91-update-grub.hook
-[Trigger]
-Type = File
-Operation = Install
-Operation = Upgrade
-Operation = Remove
-Target = usr/lib/modules/*/vmlinuz
+# 	if [ ! -f /etc/pacman.d/hooks/91-update-grub.hook ]; then
+#   		cat <<EOF | sudo tee -a /etc/pacman.d/hooks/91-update-grub.hook
+# [Trigger]
+# Type = File
+# Operation = Install
+# Operation = Upgrade
+# Operation = Remove
+# Target = usr/lib/modules/*/vmlinuz
 
-[Action]
-Description = Updating grub configuration ...
-When = PostTransaction
-Exec = /usr/bin/grub-mkconfig -o /boot/grub/grub.cfg
-EOF
-	fi
-fi
+# [Action]
+# Description = Updating grub configuration ...
+# When = PostTransaction
+# Exec = /usr/bin/grub-mkconfig -o /boot/grub/grub.cfg
+# EOF
+# 	fi
+# fi
 
 # Install yay AUR helper if not already present
-if ! command -v yay &> /dev/null; then
+if ! command -v yay &>/dev/null; then
   sudo pacman -S --needed --noconfirm base-devel
   git clone https://aur.archlinux.org/yay-bin.git
   cd yay-bin
   makepkg -si
+  # Cleanup ./yay-bin directory after package is installed
+  cd ..
+  rm -rf yay-bin
 fi
 
-if ! command -v arch-update &> /dev/null ; then
-	echo "Install arch-update from AUR"
-	yay -S arch-update
-	# Add the arch-update-tray.desktop app in your XDG Autostart directory
-	cp -v /usr/share/applications/arch-update-tray.desktop $HOME/.config/autostart
+if ! command -v arch-update &>/dev/null; then
+  echo "Install arch-update from AUR"
+  yay -S arch-update
+  # Add the arch-update-tray.desktop app in your XDG Autostart directory
+  cp -v /usr/share/applications/arch-update-tray.desktop $HOME/.config/autostart
 
-	echo "Enable the arch-update systemd timer"
-	systemctl --user enable --now arch-update.timer
+  echo "Enable the arch-update systemd timer"
+  systemctl --user enable --now arch-update.timer
 fi
 
 echo "Install Uncomplicated Firewall"
 sudo pacman -S --needed --noconfirm ufw gufw
 
 # Add current user to sudoers
-echo "${USER} ALL=(ALL:ALL) ALL" | sudo tee /etc/sudoers.d/${USER}
+#echo "${USER} ALL=(ALL:ALL) ALL" | sudo tee /etc/sudoers.d/${USER}
