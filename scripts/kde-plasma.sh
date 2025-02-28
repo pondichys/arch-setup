3cdd66170d9ff3ec5b95c6ff13ee5808f37085b7
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2025-02-28 10:39:31 +0100

    feat(kde): add sddm-kcm

--- scripts/kde-plasma.sh
+++ scripts/kde-plasma.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-sudo pacman -S --needed --noconfirm plasma-desktop sddm ark kscreen konsole kde-system-meta plasma-pa plasma-nm plasma-firewall
+sudo pacman -S --needed --noconfirm plasma-desktop sddm \
+  ark kscreen konsole kde-system-meta sddm-kcm \
+  plasma-pa plasma-nm plasma-firewall
