527eff5926e92be371dbe6a215e66613439ff00f
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2025-03-13 11:22:45 +0100

    fix(print): enable cups.socket and avahi-daemon.service

--- scripts/print_scan.sh
+++ scripts/print_scan.sh
@@ -6,8 +6,11 @@ sudo usermod -aG scanner $USER
 
 # Printing
 sudo pacman -S --needed --noconfirm cups cups-pdf avahi nss-mdns
+
+sudo systemctl enable --now cups.socket
 # Enable avahi hostname resolution
 # Edit /etc/nssswitch.conf and replace the line
 # hosts: mymachines resolve [!UNAVAIL=return] files myhostname dns
 # by 
 # hosts: mymachines mdns_minimal [NOTFOUND=return] resolve [!UNAVAIL=return] files myhostname dns
+sudo systemctl enable --now avahi-daemon.service
