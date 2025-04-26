ec2b005120e0f71b54a0311e6ae26a8391d52392
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2025-04-26 10:00:36 +0200

    feat(plasma): add plasma-addons for more task switcher options

--- scripts/kde-plasma.sh
+++ scripts/kde-plasma.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo pacman -S --needed --noconfirm plasma-desktop sddm \
+sudo pacman -S --needed --noconfirm plasma-desktop kdeplasma-addons sddm \
   ark kscreen konsole kde-system-meta print-manager sddm-kcm \
   plasma-pa plasma-nm plasma-firewall
 
